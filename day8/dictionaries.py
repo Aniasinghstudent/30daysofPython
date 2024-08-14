7ee7e709c3ee17a14c8cd6cc6e55f1587d36f0fa
Ania <mail2aniasingh@gmail.com>
2024-08-13 21:26:23 -0400

    finished it

---
+++ day8/dictionaries.py
@@ -0,0 +1,22 @@
+dog = {
+    'name:loki'
+    'colour:brown'
+    'legs:4'
+    'age:3'
+    }
+student_dict = {
+    'first_name':'Ania',
+    'last_name':'Singh',
+    'gender':'female',
+    'age':'14',
+    'is_married':'False',
+    'skills':['python','thats literally it'],
+    'country':'Norway',
+    'address':'hickory place way 5434',
+    'city':'Glen Allen'
+}
+
+print(len(student_dict))
+
+values = student_dict.keys
+print(values)
